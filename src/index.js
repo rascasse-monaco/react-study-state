72196fe4b2bd93577cfff8996b4f1541b440784d
rascasse-monaco <mktwkhr@gmail.com>
2021-01-30 02:55:14 +0900

    classコンポーネントに変更

--- src/index.js
+++ src/index.js
@@ -1,28 +1,52 @@
-import React, { useState } from 'react';
+//import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Card from 'react-bootstrap/Card';
 import Form from 'react-bootstrap/Form';
-//import React from 'react';
+import React from 'react';
 //import { useState } from 'react';
-
-const Hello = (props) => {
-  const [profile, setProfile] = useState({
-    name: "name"
-  });
+class Hello extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "World"
+    };
+  }
+  render() {
     return (
-        <Card>
-            <Card.Body>
-                <h1>Name: {profile.name}!</h1>
-                <Form.Control
-                  value={profile.name}
-                  onChange={(e) => {
-                    setProfile({ name: e.target.value })
-                  }} />
-            </Card.Body>
-        </Card>
-    )
+      <Card>
+        <Card.Body>
+          <h1>Hello {this.state.name}!</h1>
+          <Form.Control
+            value={this.state.name}
+            onChange={(e) => {
+              this.setState({
+                name: e.target.value
+              })
+            }} />
+        </Card.Body>
+      </Card>
+    );
+  }
 }
+
+//const Hello = (props) => {
+//  const [profile, setProfile] = useState({
+//    name: "World"
+//  });
+//    return (
+//        <Card>
+//            <Card.Body>
+//                <h1>Hello {profile.name}!</h1>
+//                <Form.Control
+//                  value={profile.name}
+//                  onChange={(e) => {
+//                    setProfile({ name: e.target.value })
+//                  }} />
+//            </Card.Body>
+//        </Card>
+//    )
+//}
 
 let dom = document.getElementById('root');
 ReactDOM.render(
